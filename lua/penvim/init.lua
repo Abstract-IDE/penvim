4aac2a7be3fa9684e2f6a4ddb7c0eaa9912a842c
Ali Shahid <canttell@gmail>
2022-06-17 14:25:16 +0545

    [rooter/init.lua] expand project root dirs lists

--- lua/penvim/init.lua
+++ lua/penvim/init.lua
@@ -14,7 +14,37 @@ local default = {
 
 	rooter = {
 		enable = true,
-		patterns = {'.__nvim__.lua', '.git', 'node_modules', '.sln', '.svn', 'wwwroot'}
+		patterns = {
+			'.__nvim__.lua', '.git', 'node_modules', '.sln', '.svn', 'wwwroot', '.projectile',
+			'rebar.config', -- Rebar project file
+			'project.clj', --Leiningen project file
+			'build.boot', --Boot-clj project file
+			'deps.edn', --Clojure CLI project file
+			'SConstruct', --Scons project file
+			'default.nix', --Nix project file
+			'flake.nix', --Nix flake project file
+			'pom.xml', --Maven project file
+			'build.sbt', --SBT project file
+			'build.sc', --Mill project file
+			'gradlew', --Gradle wrapper script
+			'build.gradle', --Gradle project file
+			'.ensime', --Ensime configuration file
+			'Gemfile', --Bundler file
+			'requirements.txt', --Pip file
+			'setup.py', --Setuptools file
+			'tox.ini', --Tox file
+			'composer.json', --Composer project file
+			'Cargo.toml', --Cargo project file
+			'mix.exs', --Elixir mix project file
+			'stack.yaml', --Haskell’s stack tool based project
+			'dune-project', --OCaml Dune project file
+			'info.rkt', --Racket package description file
+			'DESCRIPTION', --R package description file
+			--TAGS etags/ctags are usually in the root of project', --GTAGS GNU Global tags
+			'configure.in', --autoconf old style
+			'configure.ac', --autoconf new style
+			'cscope.out', --cscope
+		}
 	},
 
 	-- working on it ...
