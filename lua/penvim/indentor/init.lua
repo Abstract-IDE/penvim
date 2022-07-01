74bd642b567c1ee1bc8d19034852e9517ab21e86
Ali Shahid <canttell@gmail>
2022-07-01 15:07:15 +0545

    [indentor] working...

--- lua/penvim/indentor/init.lua
+++ lua/penvim/indentor/init.lua
@@ -26,5 +26,72 @@ end
 -- )
 --
 
+-- local function whitespace_count(buff, type, line)
+-- 	-- buff=buffer number | type=tab/space | line=line number
+-- 	if type == "tab" then
+-- 		local math_pattern = "^%\t*"
+-- 	else
+-- 		local math_pattern = "^%s*"
+-- 	end
+-- end
+
+local function whitespace_type (line)
+
+	local whitespace = line:match("^%s*"):len()
+	if whitespace == 0 then
+		return 'blank'
+	end
+	local tabspace = line:match("^%\t*"):len()
+	if tabspace > 0 then
+		return {
+			type = "tab",
+			no_of_tab = tabspace
+		}
+	end
+
+	return {
+		type = "space",
+		no_of_space = whitespace
+	}
+end
+
+local get_lines = function (buff, start, last)
+	vim.api.nvim_buf_get_lines(buff, start, last, {})
+end
+
+
+-- local get_line = function (line_num)
+-- 	return vim.fn.getline(line_num)
+-- end
+
+local function logic()
+	local loc = 1
+	local current_line_num, current_line_content, whitespace
+	local next_line_num = current_line_num + 1
+	local stack_tab = 0
+	local stack_space = 0
+	local space_list = {}
+	if loc == 1 then
+		current_line_num = 0
+		next_line_num = 1
+	end
+
+	for line_num = current_line_num, next_line_num do
+		current_line_content = get_lines(buffer_num, line_num, line_num+1)
+		whitespace = whitespace_type(current_line_content)
+		type = whitespace.type
+
+		if whitespace ~= 'blank' then
+			if type == "tab" then
+				stack_tab = stack_tab + 1
+			else
+				stack_space = stack_space + 1
+				space_list[#space_list+1] = whitespace.no_of_space
+			end
+		end
+	end
+
+end
+
 return M
 
