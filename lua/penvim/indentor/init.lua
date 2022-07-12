27ade045ba3156689229bcde16e13ee2ad4645a3
Ali Shahid <canttell@gmail>
2022-07-12 14:48:54 +0545

    [indentor] implement

--- lua/penvim/indentor/init.lua
+++ lua/penvim/indentor/init.lua
@@ -2,13 +2,11 @@
 local M = {}
 
 
-local function whitespace_type (line)
+local function whitespace_type(line)
 
 	local whitespace = line:match("^%s*"):len()
 	if whitespace == 0 then
-		return {
-			type = "blank",
-		}
+		return { type = "blank", }
 	end
 
 	local tabspace = line:match("^%\t*"):len()
@@ -26,76 +24,165 @@ local function whitespace_type (line)
 end
 
 
+local function block_comment(line)
+	-- function to check if current line is the start of block comment
+
+	local white_space = " "
+	::loop::
+	local space = line:match("^%"..white_space)
+	if space == white_space then
+		white_space = white_space .. " "
+		goto loop
+	end
+
+	local c_style = line:match("^/%*") -- /*
+	local html_style = line:match("^<!%-%-") -- <!--
+	local python_style = line:match("^\"%\"%\"") or line:match("^\'%\'%\'") -- """
+	local lua_style = line:match("^-%-%[%[") -- --[[  ]]
+
+	if c_style then return "*/$"
+	elseif html_style then return "%-%->$"
+	elseif lua_style then return "%]%]$"
+	elseif python_style then return "\"%\"%\"$"
+	else return nil
+	end
+end
+
+
+local function match_pattern(line, pattern)
+	return line:match(pattern)
+end
+
+
 local function init_load_indentor()
 
 	local buffer_num = vim.api.nvim_get_current_buf() -- current buffer number
-	local loc = vim.api.nvim_buf_line_count(buffer_num)
+	local loc = vim.api.nvim_buf_line_count(buffer_num) -- total lines of code in current file
 	local current_line_num = 1
 	local current_line_content, whitespace, whitespace_t
 	local stack_tab = 0
 	local stack_space = 0
 	local space_list = {}
+	local accuracy = vim.g.penvim_indentor_accuracy
 
 	if loc == 1 then
-		current_line_num = 0
+		goto end_loop
 	end
 
-	for line_num = current_line_num, loc do
-		current_line_content = vim.fn.getline(line_num)
-		whitespace = whitespace_type(current_line_content)
-		whitespace_t = whitespace['type']
+	::continue_loop::
 
-		if whitespace_t ~= 'blank' then
-			if whitespace_t == "tab" then
-				stack_tab = stack_tab + 1
-			else
-				stack_space = stack_space + 1
-				space_list[#space_list+1] = whitespace.no_of_space
+	if current_line_num > loc or stack_space > accuracy or stack_tab > accuracy then
+		goto end_loop
+	end
+
+	current_line_content = vim.fn.getline(current_line_num)
+
+        -- operations for BLOCK COMMENT
+        -----------------------------------------------
+	Block_comment = block_comment(current_line_content)
+
+	-- if line is not a block comment handle to operation to whitespace
+	if not Block_comment then
+		goto whitespace
+	end
+
+	-- check if block comment exist on same single line
+	if match_pattern(current_line_content, Block_comment) then
+		current_line_num = current_line_num + 1
+		goto continue_loop
+	end
+
+	-- if Block Comment then Operation until Right pair of comment is found
+	if Block_comment then
+		for _=current_line_num, loc do
+                        current_line_num = current_line_num + 1
+			current_line_content = vim.fn.getline(current_line_num)
+			if match_pattern(current_line_content, Block_comment) then
+				current_line_num = current_line_num + 1
+				goto continue_loop
 			end
 		end
-		if stack_space >= 5 or stack_tab >= 5 then
-			break
-		end
 	end
+        -----------------------------------------------
+
+        -- operations for WHITESPACE
+        -----------------------------------------------
+        ::whitespace::
+	whitespace = whitespace_type(current_line_content)
+	whitespace_t = whitespace['type']
+
+	if whitespace_t == "blank" then
+		current_line_num = current_line_num + 1
+		goto continue_loop
+	end
+	if whitespace_t == "tab" then
+		stack_tab = stack_tab + 1
+		current_line_num = current_line_num + 1
+		goto continue_loop
+	end
+	if whitespace_t == "space" then
+		stack_space = stack_space + 1
+		space_list[#space_list+1] = whitespace.no_of_space
+		current_line_num = current_line_num + 1
+		goto continue_loop
+	end
+        -----------------------------------------------
+
+	::end_loop::
 
 	local indent_length = vim.g.penvim_indentor_length
 	local tab_set = false
 	local space_set = false
 
-	-- set tab
+        vim.opt.smarttab = true             -- <tab>/<BS> indent/dedent in leading whitespace
+        vim.opt.autoindent = true           -- maintain indent of current line
+
 	if stack_tab > stack_space then
-		vim.opt.softtabstop = indent_length
-		vim.opt.shiftwidth = indent_length  -- spaces per tab (when shifting), when using the >> or << commands, shift lines by 4 spaces
+		-- set tab
 		vim.opt.tabstop = indent_length     -- spaces per tab
-		vim.opt.smarttab = true             -- <tab>/<BS> indent/dedent in leading whitespace
-		vim.opt.autoindent = true           -- maintain indent of current line
+		vim.opt.shiftwidth = indent_length  -- spaces per tab (when shifting), when using the >> or << commands, shift lines by 4 spaces
+		vim.opt.softtabstop = indent_length -- Number of spaces that a <Tab> counts for while performing editing operations, like inserting a <Tab> or using <BS>.
+		vim.opt.expandtab = false           -- Always uses spaces instead of tab characters (et).
 		tab_set = true
 	end
-
-	-- set space
 	if stack_space > stack_tab then
-		-- TODO
-		local space_length = math.max(unpack(space_list))
+		-- set space
+		local space_length
+		if #space_list <= 1 then
+                        space_length = space_list[1]
+		else
+                        space_length = math.min(unpack(space_list))
+		end
+
+		vim.opt.tabstop = space_length     -- Size of a hard tabstop (ts).
+		vim.opt.shiftwidth = space_length  -- Size of an indentation (sw).
+		vim.opt.softtabstop = 0            -- Number of spaces a <Tab> counts for. When 0, featuer is off (sts).
+		vim.opt.expandtab = true           -- Always uses spaces instead of tab characters (et).
 		space_set = true
 	end
 
-	if not tab_set or not space_set then
+        -- TODO
+	if not tab_set and not space_set then
 		-- use default
 		local indent_type = vim.g.penvim_indentor_type   -- default auto,  auto|space|tab
 
 		if indent_type == "tab" then
+			-- TAB
 			vim.opt.softtabstop = indent_length
 			vim.opt.shiftwidth = indent_length  -- spaces per tab (when shifting), when using the >> or << commands, shift lines by 4 spaces
-			vim.opt.tabstop = indent_length     -- spaces per tab
-			vim.opt.smarttab = true             -- <tab>/<BS> indent/dedent in leading whitespace
-			vim.opt.autoindent = true           -- maintain indent of current line
-		end
-		if indent_type == "space" then
-			-- TODO
+			vim.opt.tabstop = indent_length     -- Size of a hard tabstop (ts).
+		elseif indent_type == "space" then
+			-- SPACE
+			vim.opt.softtabstop = 0 -- Number of spaces a <Tab> counts for. When 0, featuer is off (sts).
+			vim.opt.expandtab = true    -- Always uses spaces instead of tab characters (et).
+			vim.opt.shiftwidth = indent_length  -- spaces per tab (when shifting), when using the >> or << commands, shift lines by 4 spaces
+			vim.opt.tabstop = indent_length     -- Size of a hard tabstop (ts).
+		else
+			-- TODO --
+			-- auto
 			return -- to ignore error
 		end
 	end
-
 end
 
 
--- lua/penvim/init.lua
+++ lua/penvim/init.lua
@@ -50,9 +50,13 @@ local default = {
 
 	-- indentor
 	indentor = {
-		enable = true,
-		indent_length = 4,
-		indent_type = "auto" -- if file is new or it doesn't have any indentation (auto, tab, space)
+		enable = true, -- enable/disable indentor
+		indent_length = 4, -- tab indent width or no of spaces in case of indent_type
+		indent_type = "auto", -- if file is new or it doesn't have any indentation (auto, tab, space)
+		accuracy = 5, -- positive integer. higher the number, the more accurate result (but affects the startup time)
+		disable_types = {
+			'help','dashboard','dashpreview','NvimTree','vista','sagahover'
+		},
 	},
 }
 
@@ -73,6 +77,12 @@ function M.setup(options)
 	vim.g.penvim_indentor_enable = default.indentor.enable
 	vim.g.penvim_indentor_length = default.indentor.indent_length
 	vim.g.penvim_indentor_indent = default.indentor.indent_type
+	vim.g.penvim_indentor_accuracy = default.indentor.accuracy
+	local disable_types = default.indentor.disable_types
+
+	--
+	local filetype = vim.bo.filetype
+	local buftype = vim.bo.buftype
 
 
 	-- overide default options with user-defined options
@@ -102,11 +112,10 @@ function M.setup(options)
 				local default_pattern = default.rooter.patterns
 				local option_pattern = options.rooter.patterns
 
-				for _, value in ipairs(option_pattern) do
+				for _, value in pairs(option_pattern) do
 					default_pattern[#default_pattern+1] = value
 				end
 				vim.g.penvim_rooter_patterns = default_pattern
-
 			end
 		end
 
@@ -120,6 +129,17 @@ function M.setup(options)
 			end
 			if options.indentor.indent_type ~=nil then
 				vim.g.penvim_indentor_indent = options.indentor.indent_type
+			end
+			if options.indentor.accuracy ~=nil then
+				vim.g.penvim_indentor_accuracy = options.indentor.accuracy
+			end
+			if options.indentor.disable_types ~=nil then
+				local default_disable_types = default.indentor.disable_types
+				local option_disable_types = options.indentor.disable_types
+				for _, value in pairs(option_disable_types) do
+					default_disable_types[#default_disable_types+1] = value
+				end
+				disable_types = default_disable_types
 			end
 		end
 	end
@@ -170,6 +190,14 @@ function M.setup(options)
 	-- indentor
 	---------------------------------------
 	if vim.g.penvim_indentor_enable then
+
+		-- don't load indentor if filetype is passed in options
+		for _, ft in pairs(disable_types) do
+			if ft==filetype or ft==buftype then
+                                return
+                        end
+		end
+
 		vim.api.nvim_create_autocmd(
 			"BufEnter",
 			{
